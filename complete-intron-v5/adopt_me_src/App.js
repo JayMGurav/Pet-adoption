e3bf808bea8b7fc177c744cd72b890a8bb58946a
Jay <mrithyunjay2799@gmail.com>
2019-08-29 09:59:57 +0530

    convert project to use JSX, React-specific ESLint-onfig

--- complete-intron-v5/adopt_me_src/App.js
+++ complete-intron-v5/adopt_me_src/App.js
@@ -3,11 +3,19 @@ import React from "react";
 import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!!"),
-    React.createElement(Pet, { name: "Luna", type: "Dog", Breed: "Blue Eyed" }),
-    React.createElement(Pet, { name: "Mars", type: "Dog", Breed: "Pug" }),
-    React.createElement(Pet, { name: "Bruno", type: "Dog", Breed: "Dolmetian" })
-  ]);
+  // return React.createElement("div", {}, [
+  //   React.createElement("h1", {}, "Adopt Me!!"),
+  //   React.createElement(Pet, { name: "Luna", type: "Dog", Breed: "Blue Eyed" }),
+  //   React.createElement(Pet, { name: "Mars", type: "Dog", Breed: "Pug" }),
+  //   React.createElement(Pet, { name: "Bruno", type: "Dog", Breed: "Dolmetian" })
+  // ]);
+  return (
+    <div>
+      <h1 id="something-important">Adopt me!!</h1>
+      <Pet name="Luna" type="Dog" Breed="Blue Eyed" />
+      <Pet name="Mars" type="Dog" Breed="Pug" />
+      <Pet name="Bruno" type="Dog" Breed="German" />
+    </div>
+  );
 };
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
--- complete-intron-v5/adopt_me_src/Pet.js
+++ complete-intron-v5/adopt_me_src/Pet.js
@@ -1,8 +1,15 @@
 import React from "react";
 export default function Pet({ name, type, Breed }) {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, type),
-    React.createElement("h2", {}, Breed)
-  ]);
+  //   return React.createElement("div", {}, [
+  //     React.createElement("h1", {}, name),
+  //     React.createElement("h2", {}, type),
+  //     React.createElement("h2", {}, Breed)
+  //   ]);
+  return (
+    <div>
+      <h1>{name}</h1>
+      <h2>{type}</h2>
+      <h2>{Breed}</h2>
+    </div>
+  );
 }
